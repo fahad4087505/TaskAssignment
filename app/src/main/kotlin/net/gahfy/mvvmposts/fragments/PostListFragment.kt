d6cd2c64cf84e1784577a6a7f26fb2ab080ce308
fahad4087505 <fahad4087505@gmail.com>
2020-05-16 23:49:34 +0500

    Posts Work

--- app/src/main/kotlin/net/gahfy/mvvmposts/fragments/PostListFragment.kt
+++ app/src/main/kotlin/net/gahfy/mvvmposts/fragments/PostListFragment.kt
@@ -1,29 +1,40 @@
 package net.gahfy.mvvmposts.ui.post
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import net.gahfy.mvvmposts.R
+import net.gahfy.mvvmposts.base.BaseFragment
 import net.gahfy.mvvmposts.databinding.ActivityPostListBinding
-import net.gahfy.mvvmposts.injection.ViewModelFactory
-class PostListActivity: AppCompatActivity() {
+class PostListActivity: BaseFragment() {
     private lateinit var binding: ActivityPostListBinding
     private lateinit var viewModel: PostListViewModel
     private var errorSnackbar: Snackbar? = null
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_post_list)
-        binding.postList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        viewModel = ViewModelProviders.of(this, ViewModelFactory(this)).get(PostListViewModel::class.java)
+//    override fun onCreate(savedInstanceState: Bundle?){
+//        super.onCreate(savedInstanceState)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_post_list)
+//
+//    }
+
+    override fun onCreateChildView(inflater: LayoutInflater?, parent: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater!!, R.layout.activity_post_list, parent, false)
+        viewModel = ViewModelProviders.of(this).get(PostListViewModel::class.java)
+        binding.postList.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         viewModel.errorMessage.observe(this, Observer {
             errorMessage -> if(errorMessage != null) showError(errorMessage) else hideError()
         })
         binding.viewModel = viewModel
+        return binding.root
     }
+
     private fun showError(@StringRes errorMessage:Int){
         errorSnackbar = Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_INDEFINITE)
         errorSnackbar?.setAction(R.string.retry, viewModel.errorClickListener)
